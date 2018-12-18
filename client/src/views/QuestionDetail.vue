b5b829e896880fbc7811ad4739233934bf29fd75
Kevin Wijaya <kevin.tanuhardi@gmail.com>
2018-12-18 17:22:41 +0700

    upvote downvote logic done

--- client/src/views/QuestionDetail.vue
+++ client/src/views/QuestionDetail.vue
@@ -4,10 +4,10 @@
         <hr>
         <div class="row">
             <div class="col-2 d-flex flex-column">
-                <i class="fas fa-caret-up fa-3x vote"></i>
+                <i class="fas fa-caret-up fa-3x vote" v-if="!upVoted" @click='upvoteQuestion'></i>
                 <!-- isi dengan firebase -->
-                0
-                <i class="fas fa-caret-down fa-3x vote"></i>
+                {{votes}}
+                <i class="fas fa-caret-down fa-3x vote"  v-if="!downVoted" @click.prevent='downvoteQuestion'></i>
             </div>
             <div class="col-10 text-left">
                 <p>{{question.content}}</p>
@@ -50,14 +50,28 @@ export default {
             newQuestion: '',
             question: {},
             questionId : this.$route.params.id,
-            currentUser: localStorage.getItem('userName'),
+            currentUserName: localStorage.getItem('userName'),
+            currentUserEmail: localStorage.getItem('userEmail'),
             answers: {},
-            answersLength: 0
+            answersLength: 0,
+            upvoters: [],
+            downvoters: [],
+            upVoted: false,
+            downVoted: false,
+            ownUpvoteId: '',
+            ownDownvoteId: '',
+        }
+    },
+    computed: {
+        votes(){
+            return this.upvoters.length - this.downvoters.length
         }
     },
     created(){
         this.fetchQuestionDetail()
         this.fetchAnswer()
+        this.fetchQuestionUpvotes()
+        this.fetchQuestionDownvotes()
     },
     methods: {
         fetchQuestionDetail(){
@@ -66,7 +80,6 @@ export default {
                 url: 'questions/' + this.questionId
             })
             .then(({data}) =>{
-                console.log(data)
                 this.question = data.question
             })
             .catch(({response}) =>{
@@ -77,17 +90,105 @@ export default {
             db.ref(this.questionId + "/answers")
             .push({
                 content: this.newQuestion,
-                author: this.currentUser
+                author: this.currentUserName
             })
         },
         fetchAnswer() {
             let self = this
-            console.log(this.questionId + "/answers")
             db.ref(this.questionId + "/answers")
             .on('value', (snapshot) => {
                 self.answersLength = snapshot.numChildren()
                 self.answers = snapshot.val()
             })
+        },
+        upvoteQuestion(){
+            if(this.downVoted){
+                db.ref(this.questionId + "/downvote/" + this.ownDownvoteId)
+                .remove()
+                this.downVoted = false
+                this.fetchQuestionDownvotes()
+            }
+            else {
+                db.ref(this.questionId + "/upvote")
+                .push(this.currentUserEmail)
+            }
+        },
+        fetchQuestionUpvotes(){
+            let self = this
+            db.ref(this.questionId + "/upvote")
+            .on('value', (snapshot) => {
+                if(snapshot.val()){
+                    self.upvoters = Object.values(snapshot.val())
+                    for(let key in snapshot.val()){
+                        if(snapshot.val()[key] === self.currentUserEmail){
+                            self.ownUpvoteId = key
+                        }
+                    }
+                }
+                else {
+                    self.ownUpvoteId = ''
+                    self.upvoters = []
+                }
+
+                self.checkUpvote();
+            })
+        },
+        downvoteQuestion(){
+            if(this.upVoted){
+                db.ref(this.questionId + "/upvote/" + this.ownUpvoteId)
+                .remove()
+                this.upVoted = false
+                this.fetchQuestionUpvotes()
+            }
+            else {
+                db.ref(this.questionId + "/downvote")
+                .push(this.currentUserEmail)
+            }
+        },
+        fetchQuestionDownvotes(){
+            let self = this
+            db.ref(this.questionId + "/downvote")
+            .on('value', (snapshot) => {
+                if(snapshot.val()){
+                    self.downvoters = Object.values(snapshot.val())
+                    for(let key in snapshot.val()){
+                        if(snapshot.val()[key] === self.currentUserEmail){
+                            self.ownDownvoteId = key
+                        }
+                    }
+                }
+                else {
+                    self.ownDownvoteId = ''
+                    self.downvoters = []
+                }
+                console.log('sampe sini')
+                console.log(self.che)
+                self.checkDownvote();
+            })
+        },
+        checkUpvote(){
+            let upVoted = this.upvoters.find((element) =>{
+                // console.log(element);
+                return element == this.currentUserEmail
+            })
+            
+            console.log(upVoted)
+            
+            if(upVoted){
+                this.upVoted = true
+            }
+            
+        },
+        checkDownvote(){
+            let downVoted = this.downvoters.find((element) =>{
+                return element == this.currentUserEmail
+            })
+            console.log('testt')
+            console.log(downVoted, 'down')
+
+            if(downVoted){
+                this.downVoted = true
+            }
         }
     }
 }
